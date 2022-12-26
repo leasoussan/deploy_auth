0f5ca94d9b01f0bb4cfd14667e257c4dc88b575a
lea <leampdi@gmail.com>
2022-12-26 11:51:11 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import db from './config/elephantsql.js';
+import users_router from './routes/Users.js';
+import path from 'path';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(users_router);
+
+app.listen(process.env.PORT||8080, ()=>{
+  console.log(`run on ${process.env.PORT||8080}`);
+})
+
+try {
+  await db.authenticate();
+  console.log('Database conneted... ');
+}
+catch(e){
+  console.log(e);
+}
+
+
+
+// here we add allt he dire- for de tedploy 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', (req,res)=>{
+  res.sendFile(path.resolve(__dirname,'./client/build', 'index.html'));
+})
